e81f5c98a62207ec44a8a28c16d14f2adca5deec
dennis woolsey <dennis_woolsey@sbcglobal.net>
2015-03-26 16:38:20 -0700

    Committed by Udacity

---
+++ Pitch Perfect Rev 2/Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,109 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by User on 3/11/15.
+//  Copyright (c) 2015 Neural Health Systems. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer!
+    var receivedAudio:RecordedAudio!
+    
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
+
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
+        audioPlayer.enableRate = true
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
+        
+        // Sets sound to always play on speakers
+        let session = AVAudioSession.sharedInstance()
+        var error:NSError?
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: &error)
+        session.overrideOutputAudioPort(AVAudioSessionPortOverride.Speaker, error: &error)
+        session.setActive(true, error: &error)
+        
+    
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playSlow(sender: UIButton) {
+        
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        audioPlayer.rate = 0.5
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+
+    @IBAction func playFast(sender: UIButton) {
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        audioPlayer.rate = 1.5
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+    
+    @IBAction func stopButton(sender: UIButton) {
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+    }
+    
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        playAudioWithVariablePitch(1000)
+    }
+    
+    @IBAction func playDarthvaderAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        playAudioWithVariablePitch(-1000)
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
+    }
+
+}
